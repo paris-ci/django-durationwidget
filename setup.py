570940b88abe15e00ff69b20a02625846a79e50e
Arthur <site-github@api-d.com>
2023-03-24 15:46:46 +0100

    fr

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-durationwidget",
-    version="1.0.8",
+    version="1.0.9",
     author="Devang Padhiyar",
     author_email="devangpadhiyar700@gmail.com",
     description="Django Duration field widget to handle duration field in the form",
